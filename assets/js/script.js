f2de59587fb41c0327fe0fcc4c7960f6afbf449b
Dominic <dominic01tel@gmail.com>
2026-08-06 02:54:41 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,9 +57,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    // ==========================================
     // 3. LOGIKA MODAL SPLIT VIEW (LAPORAN)
-    // ==========================================
     const cards = document.querySelectorAll('.laporan-card');
     const modal = document.getElementById('split-modal');
     const closeModal = document.querySelector('.close-modal');
@@ -78,7 +76,6 @@ document.addEventListener("DOMContentLoaded", function() {
             // Masukkan data ke dalam Modal pop-up
             modalTitle.textContent = title;
             
-            // JURUS ANTI-CACHE: Menambahkan waktu saat ini agar browser selalu memuat ulang PDF (Tidak pakai memori lama)
             // Mengambil file PDF dan menyembunyikan Toolbar/Menu bawaan browser
             modalPdf.src = pdfSrc + "#toolbar=0&navpanes=0&scrollbar=0";
             
@@ -125,9 +122,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    // ==========================================
     // 4. SCROLL REVEAL (INTERSECTION OBSERVER)
-    // ==========================================
     
     // Pengaturan sensor: elemen akan bereaksi jika 15% bagiannya sudah terlihat di layar
     const observerOptions = {
@@ -139,15 +134,14 @@ document.addEventListener("DOMContentLoaded", function() {
         entries.forEach(entry => {
             // Jika elemen masuk ke dalam layar
             if (entry.isIntersecting) {
-                entry.target.classList.add('active'); // Jalankan animasi masuk
+                entry.target.classList.add('active'); 
             } else {
                 // Jika elemen keluar dari layar (dilewati atas/bawah)
-                entry.target.classList.remove('active'); // Kembalikan ke state awal agar bisa dianimasikan lagi
+                entry.target.classList.remove('active'); 
             }
         });
     }, observerOptions);
 
-    // Cari semua elemen yang punya class animasi, lalu pantau mereka!
     const hiddenElements = document.querySelectorAll('.reveal-fade-up, .reveal-scale-up');
     hiddenElements.forEach((el) => {
         scrollObserver.observe(el);
